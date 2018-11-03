38ee904ad15ee2629e8f593cf3c7305369015fd2
WendyHuang <bh486@cornell.edu>
2018-11-03 15:05:36 -0400

    add http request to client program 2

--- ClientProgram_2/client_program2.py
+++ ClientProgram_2/client_program2.py
@@ -1,6 +1,20 @@
 #This Client Program 2 (Always Listening) receive JSON from Buffer program and record sign-in status.
 #It also speak out with the result.
 import json
+import requests
+import sys
+
+API_ENDPOINT_CheckInData = ""
+
+def get_request_checkInData():
+    try:
+        r = requests.get(API_ENDPOINT_CheckInData)
+        CheckInData = r.json()
+        r.raise_for_status()
+        return CheckInData
+    except requests.exceptions.RequestException as err:
+        print(err)
+        sys.exit(1)
 
 
 #get the check in result from the json and return the reuslt
@@ -17,3 +31,10 @@ def JsonLoad(CheckInData):
 #speak the check in result using
 def speakResult(person, CurrentTime, Status, MeetingType):
     return
+
+def main():
+    CheckInData = get_request_checkInData()
+    person, CurrentTime, Status, MeetingType = JsonLoad(CheckInData)
+    speakResult(person, CurrentTime, Status, MeetingType)
+
+main()
