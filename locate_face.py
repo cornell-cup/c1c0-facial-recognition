19f1cd2a71f4db1439eabc28e1d1662b15ac1f3e
EageRAssassin <rt389@cornell.edu>
2019-02-16 13:28:21 -0500

    locate_face completed

--- locate_face.py
+++ locate_face.py
@@ -1,3 +1,36 @@
 import face_recognition
-image = face_recognition.load_image_file("your_file.jpg")
-face_locations = face_recognition.face_locations(image)
+from PIL import Image
+
+# find largest face in the image
+# return None if no face is found or the face is too small
+# (less than 1/12 of the image)
+def find_face(image):
+    face_locations = face_recognition.face_locations(image)
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        image_x, image_y, d = image.shape
+        # minimal threshold to reject a face result
+        threshold = min(image_x, image_y) / 12
+        if right - left > threshold:
+            # a qualifying face
+            return face_location
+    return None
+
+def main():
+# capture image
+    image_unavailable = False
+    while (image_unavailable == False):
+        with picamera.PiCamera() as camera:
+            image = camera.capture("test.png")
+        face_location = find_face(image)
+        if face_location != None:
+            top, right, bottom, left = face_location
+            # check if the face is within the middle 20% of the image
+            face_center = (right + left) / 2
+            image_x, image_y, d = image.shape
+            if face_center > image_x * 0.4 and face_center < image_x * 0.6:
+                # crop image
+                break
+        # rotate head
+
+main()
