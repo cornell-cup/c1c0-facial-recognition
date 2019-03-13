efa54b981db61c089d8985c793bd91e95648a446
Rong Tan <rt389@cornell.edu>
2019-03-13 21:43:25 +0000

    locate_face is not the new client

--- locate_face.py
+++ locate_face.py
@@ -1,3 +1,9 @@
+import json
+import requests
+import sys
+import io
+import socket
+import picamera
 import face_recognition
 from PIL import Image
 
@@ -5,36 +11,87 @@ from PIL import Image
 # return None if no face is found or the face is too small
 # (less than 1/12 of the image)
 def find_face(image):
+
     face_locations = face_recognition.face_locations(image)
+    """
     for face_location in face_locations:
         top, right, bottom, left = face_location
         image_x, image_y, d = image.shape
-        # minimal threshold to reject a face result
+        # minimal threshold to reject a face result if face too small
         threshold = min(image_x, image_y) / 12
         if right - left > threshold:
             # a qualifying face
             return face_location
+    """
+    if len(face_locations) > 0:
+        return face_locations[0]
+    
     return None
 
+url = "http://10.129.3.148:11000/identify-face"
+
+def send_test_image():
+    files = {
+        "image": open("cropped.png", "rb")
+    }
+    response = requests.post(url, files=files, verify=False)
+    return response.json()
+
+#get the check in result from the json and return the reuslt
+def JsonLoad(CheckInData):
+    #temp = json.load(CheckInData)
+    temp = CheckInData
+    person = temp['name']
+    checkInStatus = temp['checkInStatus']
+    meetingType = temp['meetingType']
+    return person, checkInStatus, meetingType
+
+
+#speak the check in result using
+def speakResult(person, checkInStatus, meetingType):
+    print(person)
+    if person == 'None':
+        text = 'No such a person'
+    if person == None:
+        text = 'No such a person and person is none'
+    else:
+        text = person + 'successfully check in'
+    subprocess.check_output(['espeak','-ven-us', text])
+    
 def main():
 # capture image
-    image_unavailable = False
-    while (image_unavailable == False):
+    failure_tries = 0
+    while (failure_tries < 3):
         with picamera.PiCamera() as camera:
-            image = camera.capture("test.png")
+            camera.resolution = ( 960, 540 )
+            image = camera.capture("capture.png")
+
+        # TODO http://jireren.github.io/blog/2016/02/27/face-recognition-system-based-on-raspberry-pi-2/
+
+        image = face_recognition.load_image_file("capture.png")
         face_location = find_face(image)
+        
+        print(face_location)
+        
         if face_location != None:
             top, right, bottom, left = face_location
             # check if the face is within the middle 20% of the image
             face_center = (right + left) / 2
             image_x, image_y, d = image.shape
-            if face_center > image_x * 0.4 and face_center < image_x * 0.6:
+            if face_center > image_y * 0.3 and face_center < image_y * 0.7:
                 # crop image
                 face_image = image[top:bottom, left:right]
                 pil_image = Image.fromarray(face_image)
-                pil_image.save('/alittletest.png', 'PNG')
+                pil_image.save('cropped.png', 'PNG')
+                print("cropped")
                 break
-        # rotate head
-        print("rotate head")
-
+        else:
+            # shake head and say i cannot find you
+            failure_tries += 1
+            
+    print("detected a valid face")  
+    json_feedback = send_test_image()
+    person, checkInStatus, meetingType = JsonLoad(json_feedback)
+    speakResult(person, checkInStatus, meetingType)
+    
 main()
