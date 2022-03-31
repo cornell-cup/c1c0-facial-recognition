31e538e6d83ba6efe8e744762bc38dcca3b3ef40
Christopher De Jesus <Christopher2653@gmail.com>
2022-03-31 12:13:19 -0400

    Moved pictures into resources folder, outlines api, and added to gitignore

--- r2_facial_recognition/client/api.py
+++ r2_facial_recognition/client/api.py
@@ -1,6 +1,45 @@
+import numpy as np
+
+
+LOCAL = None
+from .local import check_faces, load_images
+
+
+def set_local(filepath: str):
+    global LOCAL
+    LOCAL = False
+    load_images()
+
+
+def set_remote(conn_info: dict):
+    global ip, port, LOCAL
+    ip = conn_info['ip']
+    port = conn_info['port']
+    LOCAL = False
+
+
+def analyze_face(img: np.ndarray):
+    if LOCAL is None:
+        raise RuntimeError('Mode unset, please call set_local or set_remote')
+    elif LOCAL:
+        pass
+    else:
+        pass
+
+
 def load_images():
-    pass
+    if LOCAL is None:
+        raise RuntimeError('Mode unset, please call set_local or set_remote')
+    elif LOCAL:
+        pass        
+    else:
+        pass
 
 
 def check_faces():
-    pass
+    if LOCAL is None:
+        raise RuntimeError('Mode unset, please call set_local or set_remote')
+    elif LOCAL:
+        pass        
+    else:
+        pass
